bc6bc7a304b7aaf3ca5cabd3d57a800b8deb2f1f
David J. Heberle <davidjheberle@gmail.com>
2018-11-10 10:05:20 -0500

    Add main scene with required components

--- Assets/Scripts/GameState/GameStateLoader.cs
+++ Assets/Scripts/GameState/GameStateLoader.cs
@@ -1,34 +1,7 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine.SceneManagement;
 
-public class GameStateLoader : MonoBehaviour
+public static class GameStateLoader
 {
-    static GameStateLoader instance;
-    
-    // Self-initialize.
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        } else
-        {
-            Destroy(this);
-            Debug.LogWarning("More than 1 GameStateLoader was initialized.");
-        }
-    }
-
-    // Cross-initialize.
-    void Start()
-    {
-        // Load the title game state is no other state is loaded.
-        if (SceneManager.sceneCount == 1)
-        {
-            // Load the title scene.
-            LoadSceneAsync("title");
-        }
-    }
-
     // Load a scene asynchronously.
     public static void LoadSceneAsync(string sceneName, LoadSceneMode loadSceneMode = LoadSceneMode.Additive)
     {
@@ -41,5 +14,12 @@ public class GameStateLoader : MonoBehaviour
     {
         //  Unload the scene.
         SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+    // Return true if a scene with the scene name is loaded.
+    public static bool IsLoaded(string sceneName)
+    {
+        Scene result = SceneManager.GetSceneByName(sceneName);
+        return result.IsValid() && result.isLoaded;
     }
 }
---
+++ Assets/Scripts/Utilities/MainLoader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainLoader : MonoBehaviour
+{
+    static MainLoader instance;
+
+    // Cross-initialize.
+    void Start()
+    {
+        // If this hasn't run yet.
+        if (instance == null)
+        {
+            instance = this;
+
+            // Load the main (required) scene if it is not loaded.
+            if (!GameStateLoader.IsLoaded("main"))
+            {
+                GameStateLoader.LoadSceneAsync("main");
+            }
+            // Else if it is loaded and it's the only scene loaded.
+            else if (SceneManager.sceneCount == 1)
+            {
+                // Load the title scene.
+                GameStateLoader.LoadSceneAsync("title");
+            }
+        }
+        else
+        {
+            // Destroy this game object.
+            Destroy(gameObject);
+        }
+    }
+}
